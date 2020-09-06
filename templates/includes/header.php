a2e6e5e72fff0300632c1e0b0c2a215cbbae2820
DanteFrank <davidfrankoziwo@gmail.com>
2020-09-06 17:43:36 +0100

    Structure SetUp

---
+++ templates/includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>JobLister</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+  <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+      <a class="navbar-brand" href="#">Dante's Web Job Lister</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse text-center" id="navbarsExample11">
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="create.php">Create Listing</a>
+              </li>
+          </ul>
+      </div>
+  </div>
+</nav>
